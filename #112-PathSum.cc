4fd05753ba3532406f0675ba58b194eae0ab1051
Jesse <jesselingxunmeng@gmail.com>
2018-04-19 23:59:23 -0400

    Create #112-PathSum.cc

---
+++ #112-PathSum.cc
@@ -0,0 +1,5 @@
+//Time Complexity: O(n) needed to pass through the number of leaves 
+//Space Complexity: O(1)
+//difficulty: easy
+
+//Further Explanation: code is pretty trivial, it goes through the tree recursively to check if any path add up to the sum
